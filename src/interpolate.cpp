7f75f865e17395787b8444e4a51060593723ccb8
Ignacio Cuñado Barral <ignacunado@gmail.com>
2023-10-21 12:52:38 +0200

    Added comments and removed TODOs

--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -12,6 +12,7 @@
 // This method is unit-tested, so do not change the function signature.
 glm::vec3 computeBarycentricCoord(const glm::vec3& v0, const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& p)
 {
+    //Compute barycentric barycentric coordinates using the normals
     glm::vec3 n = glm::cross((v1 - v0), (v2 - v0));
     glm::vec3 na = glm::cross((v2 - v1), (p - v1));
     glm::vec3 nb = glm::cross((v0 - v2), (p - v2));
@@ -32,8 +33,9 @@ glm::vec3 computeBarycentricCoord(const glm::vec3& v0, const glm::vec3& v1, cons
 // This method is unit-tested, so do not change the function signature.
 glm::vec3 interpolateNormal(const glm::vec3& n0, const glm::vec3& n1, const glm::vec3& n2, const glm::vec3 bc)
 {   
+    // Check if barycentric coordinates really represent a point inside the triangle
     assert(bc[0] < 1 && bc[0] > 0 && bc[1] < 1 && bc[1] > 0 && bc[2] < 1 && bc[2] > 0);
-    // TODO: implement this function.
+    // Use weights of barycentric coordinates to interpolate normal at each specific vertex
     return bc.x * n0 + bc.y * n1 + bc.z * n2;
 }
 
@@ -47,7 +49,7 @@ glm::vec3 interpolateNormal(const glm::vec3& n0, const glm::vec3& n1, const glm:
 // This method is unit-tested, so do not change the function signature.
 glm::vec2 interpolateTexCoord(const glm::vec2& t0, const glm::vec2& t1, const glm::vec2& t2, const glm::vec3 bc)
 {
-// TODO: implement this function.
+    // Same idea as interpolateNormal, check if valid, then use weights
     assert(bc[0] < 1 && bc[0] > 0 && bc[1] < 1 && bc[1] > 0 && bc[2] < 1 && bc[2] > 0);
     return t0 * bc.x + t1 * bc.y + t2 * bc.z;
 }
